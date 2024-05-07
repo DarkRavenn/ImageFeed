8bcc39d695f1a468ce9acf2194c228d04688978a
DarkRaven <aleksandrdugaev@gmail.com>
2024-05-07 23:43:17 +0300

    Добавлен UiTableViewCell и связь с таблицей

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Aleksandr Dugaev on 07.05.2024.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
