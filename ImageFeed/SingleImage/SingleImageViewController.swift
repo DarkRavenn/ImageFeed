5cce213505634be64cdea537a12cca7fc7366854
DarkRaven <aleksandrdugaev@gmail.com>
2024-06-01 02:06:16 +0300

    Добавлен SingleImageViewController и перход на него

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Aleksandr Dugaev on 01.06.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
