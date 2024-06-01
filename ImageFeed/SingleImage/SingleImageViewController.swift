4a3bed97373324385e4089e712d0f960cf31d70f
DarkRaven <aleksandrdugaev@gmail.com>
2024-06-01 21:35:29 +0300

    Исправлен краш из за не прогруженного аутлета

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
-    @IBOutlet weak var imageView: UIImageView!
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return } // 1
+            imageView.image = image // 2 
+        }
+    }
     
+    @IBOutlet private var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
 }
