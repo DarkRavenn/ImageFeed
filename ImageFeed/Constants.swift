eb5e96697abfc25c67f896d72c97448a0873340e
DarkRaven <aleksandrdugaev@gmail.com>
2024-06-12 14:32:46 +0300

    Добавлено перечисление Constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Aleksandr Dugaev on 12.06.2024.
+//
+
+import Foundation
+
+private enum Constants {
+    static let accessKey = "4GL81kLaodobhpvBsNvQN6oHVx6Oo9q9pP7e7RWca-s"
+    static let secretKey = "A1JPsL4Kh-WN5PScMjrMlfpZ-5ODYtVb53idcQEKLXQ"
+    static let redirectURL = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "Public+Read_user+Write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")
+}
