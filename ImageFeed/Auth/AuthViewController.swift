a6657f8e8c7550c010b618aacebd66cfd872040b
DarkRaven <aleksandrdugaev@gmail.com>
2024-07-05 00:00:43 +0300

    Изменен дизайн кнопки назад в WebViewViewController

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -8,5 +8,18 @@
 import UIKit
 
 final class AuthViewController: UIViewController {
+    private let showWebViewSegueIdentifier = "ShowSingleImage"
     
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button") // 1
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button") // 2
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil) // 3
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black") // 4
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureBackButton()
+    }
 }
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Aleksandr Dugaev on 04.07.2024.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    @IBOutlet private weak var webView: WKWebView!
+    
+}
