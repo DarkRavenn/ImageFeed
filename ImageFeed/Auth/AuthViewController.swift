e773f1e9dc17489b82a89be946b272be53e8f9fb
DarkRaven <aleksandrdugaev@gmail.com>
2024-07-04 22:06:24 +0300

    Сверстан AuthViewController

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Aleksandr Dugaev on 04.07.2024.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
