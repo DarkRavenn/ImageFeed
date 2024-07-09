854754bc8d638aae22a88da70a40c69634aeb7b2
DarkRaven <aleksandrdugaev@gmail.com>
2024-07-09 20:49:03 +0300

    Добавлена загрузка страницы авторизации в web view

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -9,7 +9,39 @@ import UIKit
 import WebKit
 
 final class WebViewViewController: UIViewController {
+    enum WebViewConstants {
+        static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    }
+    
     @IBOutlet private weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadAuthView()
+    }
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
+            print("urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) = nil")
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_url", value: Constants.redirectURL),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        
+        guard let url = urlComponents.url else {
+            print("url = urlComponents.url = nil")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
     
     @IBAction private func didTapBackButton(_ sender: Any) {
         dismiss(animated: true)
--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-private enum Constants {
+enum Constants {
     static let accessKey = "4GL81kLaodobhpvBsNvQN6oHVx6Oo9q9pP7e7RWca-s"
     static let secretKey = "A1JPsL4Kh-WN5PScMjrMlfpZ-5ODYtVb53idcQEKLXQ"
     static let redirectURL = "urn:ietf:wg:oauth:2.0:oob"
