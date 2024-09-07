c94d695fbc90fce725a54328508442fb04909b9d
DarkRaven <aleksandrdugaev@gmail.com>
2024-09-07 17:16:09 +0300

    Добавлена загрузка и отображение фото с сервера

--- ImageFeed/ImagesList/ImagesListService.swift
+++ ImageFeed/ImagesList/ImagesListService.swift
@@ -8,15 +8,14 @@
 import Foundation
 
 final class ImagesListService {
-    
     static let shared = ImagesListService()
     private init() {}
     
     static let didChangeNotification = Notification.Name(rawValue: "ImageListProviderDidChange")
+
+    private (set) var photos: [Photo] = []
     
     private var isActiveArrayPhoto = false
-    
-    private (set) var photos: [Photo] = []
     
     private let urlSession = URLSession.shared
     private var task: URLSessionTask?
@@ -24,60 +23,31 @@ final class ImagesListService {
     private let snakeCaseJSONDecoder = SnakeCaseJSONDecoder()
     private let tokenStorage = OAuth2TokenStorage()
     
-    private lazy var dataFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .long
-        formatter.timeStyle = .none
-        return formatter
-    }()
-    
-    
     private var lastLoadedPage: Int?
     
     func fetchPhotosNextPage() {
-        print("Проверяем наличие активной загрузки")
+        print("Проверяем наличие активной загрузки изображений текущая страница: \(lastLoadedPage ?? 0)")
         if task == nil {
             let nextPage = (lastLoadedPage ?? 0) + 1
-            print(nextPage)
-            print("Активной загрузки нет погнали")
+            print("Активной загрузки нет, загружаем страницу: \(nextPage)")
             fetchArrayPhoto(page: nextPage) { [weak self] result in
                 UIBlockingProgressHUD.dismiss()
                 guard let self = self else { return }
+                
                 switch result {
-                case.success(let arrayPhoto):
+                case.success(let newPhotos):
                     self.lastLoadedPage = nextPage
-                    for photo in arrayPhoto {
-//                        DispatchQueue.main.async() {
-                            self.photos.append(Photo(id: photo.id,
-                                                size: CGSize(width: photo.width,
-                                                             height: photo.height),
-                                                createdAt: self.dataFormatter.date(from: photo.createdAt),
-                                                welcomeDescription: photo.description,
-                                                thumbImageURL: photo.urls.thumb,
-                                                largeImageURL: photo.urls.full,
-                                                isLiked: photo.likedByUser))
-//                        }
-                    }
-                    NotificationCenter.default
-                        .post(name: ImagesListService.didChangeNotification,
-                              object: self,
-                              userInfo: ["Photo": photos])
-                    // TODO: Удали дебажный коммент 👇
-//                    print("""
-//                          id: \(photos[0].id)
-//                          size: \(photos[0].size)
-//                          createdAt: \(photos[0].createdAt ?? Date())
-//                          welcomeDescription: \(photos[0].welcomeDescription ?? "Сервер сказал что его нет")
-//                          thumbImageURL: \(photos[0].thumbImageURL)
-//                          largeImageURL: \(photos[0].largeImageURL)
-//                          isLiked: \(photos[0].isLiked)
-//                          """)
+                    addToPhotos(newPhotos: newPhotos)
+                    NotificationCenter.default.post(
+                        name: ImagesListService.didChangeNotification,
+                        object: self
+                    )
                 case.failure:
                     break
                 }
             }
         } else {
-            print("Есть активная загрузка отказать")
+            print("Есть активная загрузка изображений новый запрос отменен")
         }
     }
 }
@@ -96,8 +66,8 @@ extension ImagesListService {
         components?.path = UnsplashApiPath.photos
         
         let queryItems = [
-            URLQueryItem(name: "page", value: String(page)),
-            URLQueryItem(name: "per_page", value: "2"),
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "per_page", value: "10"),
         ]
 
         components?.queryItems = queryItems
@@ -145,5 +115,22 @@ extension ImagesListService {
         }
         self.task = task
         task.resume()
+    }
+    
+    private func addToPhotos(newPhotos: [PhotoResult]) {
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        
+        for photo in newPhotos {
+            let newPhoto = Photo(id: photo.id,
+                                 size: CGSize(width: photo.width, height: photo.height),
+                                 createdAt: dateFormater.date(from: photo.createdAt),
+                                 welcomeDescription: photo.description,
+                                 thumbImageURL: photo.urls.thumb,
+                                 largeImageURL: photo.urls.full,
+                                 isLiked: photo.likedByUser
+            )
+            photos.append(newPhoto)
+        }
     }
 }
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class ImagesListViewController: UIViewController {
     private let showSingleImageSegueIdentifier = "ShowSingleImage"
@@ -14,22 +15,7 @@ final class ImagesListViewController: UIViewController {
     
     @IBOutlet private var tableView: UITableView!
     
-    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
-    
-    var photos: [Photo] = [Photo(id: "jbmjneY3a6g",
-                                 size: CGSize(width: 8736.0, height: 11648.0),
-                                 createdAt: Date(),
-                                 welcomeDescription: "Сервер сказал что его нет",
-                                 thumbImageURL: "https://images.unsplash.com/photo-1721332155484-5aa73a54c6d2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MjE4NDN8MXwxfGFsbHwxfHx8fHx8Mnx8MTcyNTU3MzAxMHw&ixlib=rb-4.0.3&q=80&w=200",
-                                 largeImageURL: "https://images.unsplash.com/photo-1721332155484-5aa73a54c6d2?crop=entropy&cs=srgb&fm=jpg&ixid=M3w2MjE4NDN8MXwxfGFsbHwxfHx8fHx8Mnx8MTcyNTU3MzAxMHw&ixlib=rb-4.0.3&q=85",
-                                 isLiked: false),
-                           Photo(id: "7bQIFzFlt18",
-                                 size: CGSize(width: 2000.0, height: 3000.0),
-                                 createdAt: Date(),
-                                 welcomeDescription: "Сервер сказал что его нет",
-                                 thumbImageURL: "https://images.unsplash.com/photo-1725492123815-576fcd133766?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MjE4NDN8MHwxfGFsbHwyfHx8fHx8Mnx8MTcyNTU3MzAxMHw&ixlib=rb-4.0.3&q=80&w=200",
-                                 largeImageURL: "https://images.unsplash.com/photo-1725492123815-576fcd133766?crop=entropy&cs=srgb&fm=jpg&ixid=M3w2MjE4NDN8MHwxfGFsbHwyfHx8fHx8Mnx8MTcyNTU3MzAxMHw&ixlib=rb-4.0.3&q=85",
-                                 isLiked: false)]
+    var photos: [Photo] = []
     
     private var imageListServiceObserver: NSObjectProtocol?
     
@@ -43,60 +29,53 @@ final class ImagesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imageListServiceObserver = NotificationCenter.default    // 2
-                    .addObserver(
-                        forName: ImagesListService.didChangeNotification, // 3
-                        object: nil,                                        // 4
-                        queue: .main                                        // 5
-                    ) { [weak self] _ in
-                        guard let self = self else { return }
-                        
-//                        print("Нотификация")
-                        self.printPhotosArray()                                // 6
-                    }
-        
+        imageListServiceObserver = NotificationCenter.default.addObserver(
+            forName: ImagesListService.didChangeNotification,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            guard let self = self else { return }
+            self.updateTableViewAnimated()
+        }
+        imageListService.fetchPhotosNextPage()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom:12, right: 0)
-        
-        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == showSingleImageSegueIdentifier { //1
+        if segue.identifier == showSingleImageSegueIdentifier {
             guard
-                let viewController = segue.destination as? SingleImageViewController, // 2
-                let indexPath = sender as? IndexPath // 3
+                let viewController = segue.destination as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
             else {
-                assertionFailure("Invalid segue destination") // 4
+                assertionFailure("Invalid segue destination")
                 return
             }
             
-            let image = UIImage(named: photosName[indexPath.row]) // 5
-            viewController.image = image // 6
+            let image = UIImage(named: "scribble-placeholder")
+            viewController.image = image
         } else {
-            super.prepare(for: segue, sender: sender) // 7
+            super.prepare(for: segue, sender: sender)
         }
     }
     
-    private func printPhotosArray() {
-        for photo in imageListService.photos {
-            
-            print("""
-                  id: \(photo.id)
-                  size: \(photo.size)
-                  createdAt: \(photo.createdAt ?? Date())
-                  welcomeDescription: \(photo.welcomeDescription ?? "Сервер сказал что его нет")
-                  thumbImageURL: \(photo.thumbImageURL)
-                  largeImageURL: \(photo.largeImageURL)
-                  isLiked: \(photo.isLiked)
-                  """)
-            
+    private func updateTableViewAnimated() {
+        let oldCount = photos.count
+        let newCount = imageListService.photos.count
+        photos = imageListService.photos
+        if oldCount != newCount {
+            tableView.performBatchUpdates {
+                let indexPath = (oldCount..<newCount).map { i in
+                    IndexPath(row: i, section: 0)
+                }
+                tableView.insertRows(at: indexPath, with: .automatic)
+            } completion: { _ in }
         }
     }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return photosName.count
+        return photos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -114,11 +93,15 @@ extension ImagesListViewController: UITableViewDataSource {
 
 extension ImagesListViewController {
     func configCell(for cell: ImagesListCell, with indexPatch: IndexPath) { 
-        guard let image = UIImage(named: photosName[indexPatch.row]) else {
-            return
-        }
+        let photo = photos[indexPatch.row]
+        guard let imageUrl = URL(string: photo.thumbImageURL) else { return }
         
-        cell.cellImage.image = image
+        let cache = ImageCache.default
+        cache.clearMemoryCache()
+        cache.clearDiskCache()
+        
+        cell.cellImage.kf.indicatorType = .activity
+        cell.cellImage.kf.setImage(with: imageUrl, placeholder: UIImage(named: "scribble-placeholder"))
         
         let gradientLayer = CAGradientLayer()
         let gradientHeight = 30.0
@@ -126,10 +109,11 @@ extension ImagesListViewController {
         gradientLayer.frame = CGRect(x: 0, y: cell.frame.height - gradientHeight - marginTopAndBottom, width: cell.cellImage.bounds.width, height: gradientHeight)
         gradientLayer.colors = [UIColor.clear.cgColor, UIColor.ypBlack.withAlphaComponent(0.2).cgColor]
         gradientLayer.locations = [0.0, 1.0]
-        cell.cellImage.layer.sublayers?.forEach { $0.removeFromSuperlayer() }
+        cell.cellImage.layer.sublayers?.removeAll(where: { $0 is CAGradientLayer })
         cell.cellImage.layer.addSublayer(gradientLayer)
         
-        cell.dateLabel.text = dataFormatter.string(from: Date())
+        cell.dateLabel.text = photo.createdAt != nil ? dataFormatter.string(from: photo.createdAt!) : ""
+        
         
         let isLiked = indexPatch.row % 2 == 0
         let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
@@ -143,26 +127,19 @@ extension ImagesListViewController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard let image = UIImage(named: photosName[indexPath.row]) else {
-            return 0
-        }
+        let photo = photos[indexPath.row]
         
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
-        let imageWidth = image.size.width
+        let imageWidth = photo.size.width
         let scale = imageViewWidth / imageWidth
-        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        let cellHeight = photo.size.height * scale + imageInsets.top + imageInsets.bottom
         return cellHeight
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        // TODO: 
-        if indexPath.row + 1 == photosName.count {
-            print("Последний: \(indexPath)")
-//            imageListService.fetchPhotosNextPage()
-        } else {
-            print("Не последний: \(indexPath)")
+        if indexPath.row + 1 == imageListService.photos.count {
+            imageListService.fetchPhotosNextPage()
         }
-        
     }
 }
