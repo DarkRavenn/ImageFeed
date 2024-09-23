dd904831e423521e6b34238e467a53608e19789d
DarkRaven <aleksandrdugaev@gmail.com>
2024-09-23 23:09:09 +0300

    Добавлен ImagesListTest

---
+++ ImageFeedTests/imageList/ImagesListPresenterSpy.swift
@@ -0,0 +1,29 @@
+//
+//  ImagesListPresenterSpy.swift
+//  ImageFeedTests
+//
+//  Created by Aleksandr Dugaev on 23.09.2024.
+//
+
+import ImageFeed
+import Foundation
+
+final class ImagesListPresenterSpy: ImagesListPresenterProtocol {
+    var fetchPhotosNextPageCalled: Bool = false
+
+    var view: (any ImageFeed.ImagesListViewControllerProtocol)?
+
+    var photos: [Photo] = []
+
+    func fetchPhotosNextPage() {
+        fetchPhotosNextPageCalled = true
+    }
+
+    func didPhotosUpdate() {}
+
+    func getCellHeight(_ tableViewBoundsWidth: CGFloat, _ photoIndex: Int) -> CGFloat {
+        return 0
+    }
+
+    func imageListCellDidTapLike(_ indexPath: IndexPath) {}
+}
